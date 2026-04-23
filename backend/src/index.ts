591869c70659f78280861c5c4f5a9f17c33eda38
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-23 23:14:10 +0400

    feat: added health check for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { rootRouter } from "./routes";
 import { checkAPISecretKey, generalLimiter } from "./middlewares";
+import { Request, Response } from "express";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3008;
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(generalLimiter);
 app.use("/api", checkAPISecretKey, rootRouter);
+app.use("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
 app.listen(PORT, () => {
   console.log("It's working perfectly");
 });
