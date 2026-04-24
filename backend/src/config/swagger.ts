7954486a9effd19a0660446f2e1c5c4db5e1b366
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-25 00:45:42 +0400

    fix: first swagger doc

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -1,11 +1,33 @@
-import authDoc from '../docs/auth.docs.json'
-import config from '../docs/swagger.json'
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+import authDoc from "../docs/auth.docs.json";
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E-commerce API",
+      version: "1.0.0",
+      description: "E-Commerce REST API documentation",
+    },
+    servers: [
+      { url: "http://localhost:3008", description: "Development server" },
+      {
+        url: "https://e-commerce-full-stack-production-2d56.up.railway.app",
+        description: "Production server",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], // ← we will reference JSON from routes
+};
 
-const swaggerDocumentation = {
-    ...config,
-    paths: {
-        ...authDoc
-    }
-}
+export const swaggerSpec = {
+  ...swaggerJSDoc(options),
+  paths: {
+    ...authDoc,
+  },
+};
 
-export { swaggerDocumentation };
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
