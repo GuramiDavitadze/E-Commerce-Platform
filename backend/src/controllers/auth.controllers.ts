4c00c7ee87ccc0418842916e87886818207cffcb
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-05-18 21:37:44 +0400

    fix: changed nameSite param

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -57,7 +57,7 @@ const loginController = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ message: "Login successfully", data });
